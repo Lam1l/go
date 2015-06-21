6400c595b30c060c92e0ca9a50b5ab175a27fd9f
lam1l <atheistik@gmail.com>
2015-06-21 15:59:00 +0300

    example 23-bufferedchannels

---
+++ gobyexample/23-bufferedchannels/buffered.go
@@ -0,0 +1,16 @@
+// buffered.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	messages := make(chan string, 2)
+	
+	messages <- "buffered"
+	messages <- "channel"
+	
+	fmt.Println(<- messages)
+	fmt.Println(<- messages)
+}
